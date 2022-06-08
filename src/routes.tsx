5fb23ef00dd84968f68d8b7455bc4ad785f2a1bc
Everson Silva <everson_gsilva@yahoo.com.br>
2022-06-08 16:43:27 -0300

    fix: fix deploy

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,7 @@ const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/contrato" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
