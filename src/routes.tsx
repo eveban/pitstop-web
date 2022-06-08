71da59adf9a8499056f2ee313d29d3870f092d90
Everson Silva <everson_gsilva@yahoo.com.br>
2022-06-08 16:46:26 -0300

    fix: fix deploy

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Agreement';
+import Agreement from './pages/Agreement';
+import Home from './App';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/contrato" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contrato" element={<Agreement />} />
       </Routes>
     </BrowserRouter>
   );
