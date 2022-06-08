c7c454f81ef636bd179577b5d1db3e6bf29fbfb4
Everson Silva <everson_gsilva@yahoo.com.br>
2022-06-08 16:24:22 -0300

    fix: fix deploy

--- src/pages/Agreement/index.tsx
+++ src/pages/Agreement/index.tsx
@@ -6,7 +6,6 @@ import CurrencyInput from 'react-currency-input-field';
 import moment from 'moment';
 import { BiMailSend } from 'react-icons/bi';
 
-import { isValidCPF } from '../../utils/validaCPF';
 import { api } from '../../services/api';
 import './style.css';
 
@@ -34,6 +33,26 @@ export const Agreement: React.FC = () => {
     await api.get(
       `agreements/generated-agreement?cpf=${cpfFormatado}&dataEvento=${dataEvento}`,
     );
+  };
+
+  const isValidCPF = cpf => {
+    if (typeof cpf !== 'string') return false;
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
+    cpf = cpf.split('');
+    const validator = cpf
+      .filter((digit, index, array) => index >= array.length - 2 && digit)
+      .map(el => +el);
+    const toValidate = pop =>
+      cpf
+        .filter((digit, index, array) => index < array.length - pop && digit)
+        .map(el => +el);
+    const rest = (count, pop) =>
+      ((toValidate(pop).reduce((soma, el, i) => soma + el * (count - i), 0) *
+        10) %
+        11) %
+      10;
+    return !(rest(10, 2) !== validator[0] || rest(11, 1) !== validator[1]);
   };
 
   const onSubmit = async (data: any) => {
